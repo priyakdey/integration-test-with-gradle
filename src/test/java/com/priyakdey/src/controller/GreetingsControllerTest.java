c48e59cbca57e0d444e666965344030b7a315b56
Priyak Dey <35918976+priyakdey@users.noreply.github.com>
2021-10-13 13:39:42 +0530

    setup a new springboot project with unit test and github workflow (#1)
    * setup a new springboot project with unit test and github workflow
    
    * fix java version in workflow file

---
+++ src/test/java/com/priyakdey/src/controller/GreetingsControllerTest.java
@@ -0,0 +1,52 @@
+package com.priyakdey.src.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("UNIT - GreetingsControllerTest")
+@Tag("unit")
+@WebMvcTest(controllers = GreetingsController.class)
+class GreetingsControllerTest {
+
+    private final MockMvc mockMvc;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    GreetingsControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @DisplayName("Should return 200 OK")
+    @Test
+    void shouldReturnGreetings() throws Exception {
+        final var mockHttpServletRequestBuilder =
+                MockMvcRequestBuilders
+                        .get("/greetings")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString("Howdy stranger, how are you doing today?"));
+
+        final var response =
+                mockMvc
+                        .perform(mockHttpServletRequestBuilder)
+                        .andReturn()
+                        .getResponse();
+
+        final var returnedResponseContent = response.getContentAsString();
+        final var returnedResponseCode = response.getStatus();
+
+        assertEquals(200, returnedResponseCode);
+        assertEquals("Howdy stranger, how are you doing today?", returnedResponseContent);
+    }
+
+}
